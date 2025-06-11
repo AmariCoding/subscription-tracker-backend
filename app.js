720a7b4c757e4c93e02be45add8a2d7d3f29c636
Amari <jerome.guya@evsu.edu.ph>
2025-06-12 01:01:11 +0800

    update

--- app.js
+++ app.js
@@ -3,22 +3,34 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import subRouter from "./routes/subcription.route.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //This is from routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subRouter);
 
+//This is the middleware for handling errors
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Subscription Tracker API");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(
     `Subscription Tracker API is running on http://localhost:${PORT}`
   );
+
+  await connectToDatabase();
 });
 
 export default app;
